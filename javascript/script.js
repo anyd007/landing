78863eea7fc027fe134795ad867668b3ce9775ad
anyd007 <andy-26@wp.pl>
2018-01-09 16:55:31 +0100

    add new project

--- javascript/script.js
+++ javascript/script.js
@@ -81,119 +81,202 @@ setInterval(changeWord, 4000);
 // phone typing end
 
 // snow canvas
-
-window.onload = function() {
-
-    window.requestAnimFrame = (function() {
-
-        return  window.requestAnimationFrame       ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame    ||
-
-            function(callback) {
-                window.setTimeout(callback, 1000 / 60);
-            };
-    })();
-
-    function createSnow() {
-
-        var particles = [];
-        var particleSize = 1.75;
-        var maxParticles = 5000;
-        var particleOpacity = .9;
-
-        // Initialize canvas
-        var canvas  = document.getElementById('snow');
-        var ctx     = canvas.getContext('2d');
-
-        // Get window width & height
-        var windowWidth = window.innerWidth;
-        var windowHeight = window.innerHeight;
-
-        // Apply canvas size based on window width & height.
-        // This can be changed to bound within an element instead.
-        canvas.width = windowWidth;
-        canvas.height = windowHeight;
-
-        // Push particle iteration
-        for (var i = 0; i < maxParticles; i++) {
-
-            particles.push({
-
-                // Particle x position
-                x: Math.random() * windowWidth,
-
-                // Particle y position
-                y: Math.random() * windowHeight,
-
-                // Particle radius
-                r: Math.random(Math.min(particleSize)) * particleSize,
-
-                // Particle density
-                d: Math.random() * maxParticles,
-            });
-        }
-
-        // Render particles
-        function render() {
-
-            ctx.clearRect(0, 0, windowWidth, windowHeight);
-            ctx.fillStyle = 'rgba(255, 255, 255, ' + particleOpacity + ')';
-            ctx.beginPath();
-
-            for(var i = 0; i < maxParticles; i++) {
-
-                // Iterate the particles.
-                var p = particles[i];
-
-                // Move particles along x, y.
-                ctx.moveTo(p.x, p.y);
-
-                // Draw particle.
-                ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2, true);
-            }
-
-            ctx.fill();
-            update();
-        }
-
-        // To create a more dynamic and organic flow, we need to apply an
-        // incremental 'angle' that will iterate through each particle flow.
-        var angle = 0.005;
-
-        // Update particles
-        function update() {
-
-            // Incremental angle.
-            angle += 0.005;
-
-            for (var i = 0; i < maxParticles; i++) {
-
-                var p = particles[i];
-
-                // Offset the particles flow based on the angle.
-                p.y += Math.cos(p.d) + p.r;
-                p.x += Math.sin(angle) * Math.PI / 10;
-
-                // Re-iterate the particles to the top once the particle has
-                // reached the bottom of the window.
-                if (p.y > windowHeight) {
-
-                    particles[i] = {
-
-                        x: Math.random() * windowWidth,
-                        y: 0,
-                        r: p.r,
-                        d: p.d
-                    };
-                }
-            }
-        }
-        // Call function.
-        (function runtime() {
-            requestAnimFrame(runtime);
-            render();
-        })();
-    } createSnow();
-}
+// window.requestAnimFrame = (function() {
+//     return window.requestAnimationFrame ||
+//         window.webkitRequestAnimationFrame ||
+//         window.mozRequestAnimationFrame ||
+//         function(callback) {
+//             window.setTimeout( callback, 1000 / 60 );
+//         };
+// })();
+//
+// var canvas = document.getElementById('canvas'),
+//     ctx = canvas.getContext('2d'),
+//     cw = window.innerWidth,
+//     ch = window.innerHeight,
+//     fireworks = [],
+//     particles = [],
+//     hue = 120,
+//     limiterTotal = 50,
+//     limiterTick = 0,
+//     timerTotal = 11,
+//     timerTick = 0,
+//     mousedown = false,
+//     mx,
+//     my;
+//
+// canvas.width = cw;
+// canvas.height = ch;
+//
+// function random( min, max ) {
+//     return Math.random() * ( max - min ) + min;
+// }
+//
+// function calculateDistance( p1x, p1y, p2x, p2y ) {
+//     var xDistance = p1x - p2x,
+//         yDistance = p1y - p2y;
+//     return Math.sqrt( Math.pow( xDistance, 2 ) + Math.pow( yDistance, 2 ) );
+// }
+//
+// function Firework( sx, sy, tx, ty ) {
+//     this.x = sx;
+//     this.y = sy;
+//     this.sx = sx;
+//     this.sy = sy;
+//     this.tx = tx;
+//     this.ty = ty;
+//     this.distanceToTarget = calculateDistance( sx, sy, tx, ty );
+//     this.distanceTraveled = 0;
+//     this.coordinates = [];
+//     this.coordinateCount = 2;
+//     while( this.coordinateCount-- ) {
+//         this.coordinates.push( [ this.x, this.y ] );
+//     }
+//     this.angle = Math.atan2( ty - sy, tx - sx );
+//     this.speed = 5;
+//     this.acceleration = 500;
+//     this.brightness = random( 50, 70 );
+//     this.targetRadius = 1;
+// }
+//
+// Firework.prototype.update = function( index ) {
+//     this.coordinates.pop();
+//     this.coordinates.unshift( [ this.x, this.y ] );
+//
+//     if( this.targetRadius < 8 ) {
+//         this.targetRadius += 0.3;
+//     } else {
+//         this.targetRadius = 1;
+//     }
+//
+//     this.speed *= this.acceleration;
+//
+//     var vx = Math.cos( this.angle ) * this.speed,
+//         vy = Math.sin( this.angle ) * this.speed;
+//     this.distanceTraveled = calculateDistance( this.sx, this.sy, this.x + vx, this.y + vy );
+//
+//     if( this.distanceTraveled >= this.distanceToTarget ) {
+//         createParticles( this.tx, this.ty );
+//         fireworks.splice( index, 1 );
+//     } else {
+//         this.x += vx;
+//         this.y += vy;
+//     }
+// }
+//
+// Firework.prototype.draw = function() {
+//     ctx.beginPath();
+//     ctx.moveTo( this.coordinates[ this.coordinates.length - 1][ 0 ], this.coordinates[ this.coordinates.length - 1][ 1 ] );
+//     ctx.lineTo( this.x, this.y );
+//     ctx.strokeStyle = 'hsl(' + hue + ', 100%, ' + this.brightness + '%)';
+//     ctx.stroke();
+//
+//     ctx.beginPath();
+//     ctx.arc( this.tx, this.ty, this.targetRadius, 0, Math.PI * 2 );
+//     ctx.stroke();
+// }
+//
+// function Particle( x, y ) {
+//     this.x = x;
+//     this.y = y;
+//     this.coordinates = [];
+//     this.coordinateCount = 5;
+//     while( this.coordinateCount-- ) {
+//         this.coordinates.push( [ this.x, this.y ] );
+//     }
+//     this.angle = random( 0, Math.PI * 2 );
+//     this.speed = random( 1, 10 );
+//     this.friction = 0.95;
+//     this.gravity = 1;
+//     this.hue = random( hue - 20, hue + 20 );
+//     this.brightness = random( 50, 80 );
+//     this.alpha = 1;
+//     this.decay = random( 0.015, 0.03 );
+// }
+//
+// Particle.prototype.update = function( index ) {
+//     this.coordinates.pop();
+//     this.coordinates.unshift( [ this.x, this.y ] );
+//     this.speed *= this.friction;
+//     this.x += Math.cos( this.angle ) * this.speed;
+//     this.y += Math.sin( this.angle ) * this.speed + this.gravity;
+//     this.alpha -= this.decay;
+//
+//     if( this.alpha <= this.decay ) {
+//         particles.splice( index, 1 );
+//     }
+// }
+//
+// Particle.prototype.draw = function() {
+//     ctx. beginPath();
+//     ctx.moveTo( this.coordinates[ this.coordinates.length - 1 ][ 0 ], this.coordinates[ this.coordinates.length - 1 ][ 1 ] );
+//     ctx.lineTo( this.x, this.y );
+//     ctx.strokeStyle = 'hsla(' + this.hue + ', 100%, ' + this.brightness + '%, ' + this.alpha + ')';
+//     ctx.stroke();
+// }
+//
+// function createParticles( x, y ) {
+//     var particleCount = 230;
+//     while( particleCount-- ) {
+//         particles.push( new Particle( x, y ) );
+//     }
+// }
+//
+// function loop() {
+//     requestAnimFrame( loop );
+//     hue += 2.3;
+//
+//
+//     ctx.globalCompositeOperation = 'destination-out';
+//     ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+//     ctx.fillRect( 0, 0, cw, ch );
+//     ctx.globalCompositeOperation = 'lighter';
+//
+//     var i = fireworks.length;
+//     while( i-- ) {
+//         fireworks[ i ].draw();
+//         fireworks[ i ].update( i );
+//     }
+//
+//     var i = particles.length;
+//     while( i-- ) {
+//         particles[ i ].draw();
+//         particles[ i ].update( i );
+//     }
+//
+//     if( timerTick >= timerTotal ) {
+//         if( !mousedown ) {
+//             fireworks.push( new Firework( cw / 2, ch, random( 0, cw ), random( 0, ch / 2 ) ) );
+//             timerTick = 0;
+//         }
+//     } else {
+//         timerTick++;
+//     }
+//
+//     if( limiterTick >= limiterTotal ) {
+//         if( mousedown ) {
+//             fireworks.push( new Firework( cw / 500, ch, mx, my ) );
+//             limiterTick = 0;
+//         }
+//     } else {
+//         limiterTick++;
+//     }
+// }
+//
+// canvas.addEventListener( 'mousemove', function(e) {
+//     mx = e.pageX - canvas.offsetLeft;
+//     my = e.pageY - canvas.offsetTop;
+// });
+//
+// canvas.addEventListener( 'mousedown', function(e) {
+//     e.preventDefault();
+//     mousedown = true;
+// });
+//
+// canvas.addEventListener( 'mouseup', function( e ) {
+//     e.preventDefault();
+//     mousedown = false;
+// });
+//
+// window.onload = loop;
 // snow canvas end
